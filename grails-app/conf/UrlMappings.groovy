936f242467ecabd4d64243851ed3881f653a583a
unknown <jdelagala@94656m.sanidad.msc>
2016-01-19 16:37:07 +0100

    maqueta cabecera y limpieza de blog

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,20 +7,6 @@ class UrlMappings {
             }
         }
 
-        "/v1/posts"(resources: 'post', namespace:'blogV1')
-        "/v1/posts"(resources: 'post', namespace:'blogV1') {
-            "/comments"(controller:"post", action:"comments", method:"POST", namespace: 'blogV1')
-        }
-        "/v1/posts/tags/$tag"(controller: 'post', action: "tags", method:"GET", namespace:'blogV1')
-        "/v1/pages"(resources: 'page', namespace:'blogV1')
-        "/v1/pages/menu"(controller: 'page', action: 'menu', namespace: 'blogV1')
-        "/v1/settings/$action?"(controller: 'settings', namespace: 'blogV1')
-
-
-
-
-        "/admin/v1/pages"(resources: 'page', namespace: 'adminV1')
-        "/admin/v1/posts"(resources: 'post', namespace: 'adminV1')
         "/admin/v1/dashboard"(controller: 'dashboard', namespace: 'adminV1')
         "/admin/v1/users/$action?"(controller: 'usuario', namespace: 'adminV1')
         "/admin/v1/settings/$action?"(controller: 'settings', namespace: 'adminV1')
