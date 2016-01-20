82f204520f77baab24054164dde9b8aa026f6c05
unknown <jdelagala@94656m.sanidad.msc>
2016-01-20 08:05:19 +0100

    eliminar /admin/v1/settings/$action? de urlmappings

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -9,9 +9,6 @@ class UrlMappings {
 
         "/admin/v1/dashboard"(controller: 'dashboard', namespace: 'adminV1')
         "/admin/v1/users/$action?"(controller: 'usuario', namespace: 'adminV1')
-        "/admin/v1/settings/$action?"(controller: 'settings', namespace: 'adminV1')
-
-
 
         "/"(view:"/error")
         "/admin"(view:"/admin/index")
