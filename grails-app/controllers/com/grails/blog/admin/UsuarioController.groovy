a53850bfb3c47e9ec4699b88fd71ac99738d19ee
unknown <jdelagala@94656m.sanidad.msc>
2016-02-05 12:13:52 +0100

    UsuarioController arreglos

--- grails-app/controllers/com/grails/blog/admin/UsuarioController.groovy
+++ grails-app/controllers/com/grails/blog/admin/UsuarioController.groovy
@@ -1,6 +1,8 @@
 package com.grails.blog.admin
 
 import com.grails.blog.auth.Usuario
+import com.grails.blog.auth.Role
+import com.grails.blog.auth.UserRole
 import com.grails.blog.command.UsuarioPassword
 
 import static org.springframework.http.HttpStatus.*
@@ -46,6 +48,7 @@ class UsuarioController {
     def password(UsuarioPassword password) {
         def currentUserId = springSecurityService.getCurrentUser().id
         Usuario currentUser = Usuario.get(currentUserId)
+
         if (!currentUser) {
             render status: NOT_FOUND
             return
@@ -54,9 +57,13 @@ class UsuarioController {
         if (currentUser.password.equals(springSecurityService.encodePassword(password.currentPassword))) {
             if (password.newPassword.equals(password.confirmPassword)) {
                 currentUser.password = password.newPassword
+                /*
                 if (grailsApplication.config.blog.updates.enabled) {
                     currentUser.save()
                 }
+                */
+
+                currentUser.save()
                 render status: OK
             } else {
                 password.errors.reject(
@@ -70,13 +77,13 @@ class UsuarioController {
                 return
             }
         } else {
-            /*
+            
             password.errors.reject(
                     'userPassword.oldPassword.doesnotmatch',
                     '[Property [{0}] of class [{1}] is incorrect]')
             currentUser.save()
             render status: OK                    
-            */
+            
             println "currentUser.password: "+currentUser.password
             println "password.currentPassword: "+password.currentPassword
             println "springSecurityService.encodePassword(password.currentPassword): "+springSecurityService.encodePassword(password.currentPassword)    
