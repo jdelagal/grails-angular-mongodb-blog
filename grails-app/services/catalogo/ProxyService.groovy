7681d90534780aaab1c3ccfb1ef379025cc98c20
unknown <jdelagala@94656m.sanidad.msc>
2016-02-19 10:12:40 +0100

    credenciales en properties y recogida de llamada SOA

--- grails-app/services/catalogo/ProxyService.groovy
+++ grails-app/services/catalogo/ProxyService.groovy
@@ -15,30 +15,45 @@ import org.apache.cxf.transports.http.configuration.HTTPClientPolicy
 @Transactional
 class ProxyService {
     long connectionTimeOut = 10000L
+
     def callSOA() {
         
     	ClientProxyFactoryBean factory = new JaxWsProxyFactoryBean()
         factory.setServiceClass(BPELInventario.class)
-        factory.setAddress("http://94656m.sanidad.msc:8001/soa-infra/services/default/InventarioProyect!1.0*soa_0be81abf-2515-4dda-bcd2-916bf49dba53/bpel_inventario_client_ep")
+
+
+        Properties props = new Properties()
+        File propsFile = new File('application.properties')
+        props.load(propsFile.newDataInputStream())
+
+        def bpel_inventario_client_ep = props.getProperty('bpel_inventario_client_ep')
+        factory.setAddress("${bpel_inventario_client_ep}")
         def service = (BPELInventario) factory.create()
         Client client = ClientProxy.getClient(service)
         HTTPConduit http = (HTTPConduit) client.getConduit()
-        http.getProxyAuthorization().setUserName('jdelagala')
-        http.getProxyAuthorization().setPassword('Zamarro25')
-        HTTPClientPolicy httpClientPolicy = http.getClient(); 
-        http.getClient().setProxyServer('proxy-sanidad.msc.es')
-        http.getClient().setProxyServerPort(8080)
 
-        setUpClientProperties('proxy-sanidad.msc.es', 8080, httpClientPolicy, connectionTimeOut)
+        def username = props.getProperty('username')
+        def password = props.getProperty('password')
+        def proxyhost = props.getProperty('proxyhost')
+        
+        http.getProxyAuthorization().setUserName("${username}")
+        http.getProxyAuthorization().setPassword("${password}")
+        HTTPClientPolicy httpClientPolicy = http.getClient(); 
+
+        setUpClientProperties("${proxyhost}", 8080, httpClientPolicy, connectionTimeOut)
 
 		InventarioRequest req = new InventarioRequest()    
 		req.setCodigoProducto('codigo1')  
 		req.setNombreProducto('nombre1')  
 		req.setCategoria('categoria1')  
 
-        
+
 		InventarioResponse res = service.process(req)
+        def descripcion = res.getDetalle()
+
+        println "detalle: ${detalle}"
     }
+
 
     def setUpClientProperties(String proxyHost, Integer proxyPort, HTTPClientPolicy httpClientPolicy, long timeout) { 
         httpClientPolicy.setReceiveTimeout(timeout); 
